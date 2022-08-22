ae5ad6ca32474371bb3c7e5ff0d035532fa3c44b
josue.rivas <josu.riva@gmail.com>
2022-08-21 21:15:44 -0600

    resolucion de la guia

---
+++ Persona/Controllers/PersonasController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Persona.Models;
+
+namespace Persona.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private PersonaDBContext db = new PersonaDBContext();
+
+        // GET: Personas
+        public ActionResult Index(string buscarPersona, string buscarDui)
+        {
+            var personas = from p in db.Personas select p;
+
+            if (!string.IsNullOrEmpty(buscarPersona))
+            {
+                personas = personas.Where(p => p.Nombre.Contains(buscarPersona));
+            }
+
+            if (!string.IsNullOrEmpty(buscarDui))
+            {
+                personas = personas.Where(d => d.DUI.Equals(buscarDui));
+            }
+            return View(personas);
+        }
+
+        // GET: Personas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona.Models.Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DUI,Nombre,Apellidos,fechaNacimiento,Direccion,correo")] Persona.Models.Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personas.Add(persona);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(persona);
+        }
+
+        // GET: Personas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona.Models.Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DUI,Nombre,Apellidos,fechaNacimiento,Direccion,correo")] Persona.Models.Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(persona).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(persona);
+        }
+
+        // GET: Personas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Persona.Models.Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(persona);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Persona.Models.Persona persona = db.Personas.Find(id);
+            db.Personas.Remove(persona);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Persona/Models/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        [Key]
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode
+= true)]
+        public DateTime fechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string correo { get; set; }
+
+
+    }
+
+    public class PersonaDBContext : DbContext
+    { 
+        public DbSet<Persona> Personas { get; set; }
+    
+    }
+}
